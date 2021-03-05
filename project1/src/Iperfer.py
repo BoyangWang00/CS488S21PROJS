3e23cfcee66c928f5db2d74bc3ee0feca63538c4
Boyang Wang <wangby00@gmail.com>
2021-03-04 22:11:59 -0500

    fix bugs

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -24,18 +24,12 @@ def iperf_client(sys_argv):
         s.connect((HOST, PORT))
         data_chunk = bytearray(1000)
         timeout = time.time() + s_time
-        while 1:
+        while time.time() < timeout:
             s.sendall(data_chunk)
             chunk_counter += 1
-
-            if time.time() > timeout:
-                s.sendall(b'1')
-                #print("time is out")
-                break
-
-        s.close()
+        s.sendall(b'1')
         #print(chunk_counter, "chunks are sent")
-        rate = chunk_counter/1000/s_time
+    rate = chunk_counter/1000/s_time*8
     print('sent={0} KB rate={1} Mbps'. format(str(chunk_counter), str(rate)))
     #print('Received', repr(data))
 
@@ -50,29 +44,29 @@ def iperf_server(sys_argv):
     # Symbolic name meaning all available interfaces
     HOST = ''
     PORT = int(sys_argv[2])
-    chunk_counter = 0
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         #print("server socket is listening")
-        while True:
-            conn, addr = s.accept()
-            #print("server socket accept")
-            with conn:
-                #print('Connected by', addr)
-                time_start = time.time()
-                while True:
-                    data = conn.recv(1000)
-                    chunk_counter += 1
-                    #print("server received data")
-                    data = data.upper()
-                    if data == (b'1'):
-                        time_end = time.time()
-                        rate = chunk_counter/1000/(time_end-time_start)
-                        #print('time passed', (time_end-time_start))
-                        print('received={0} KB rate={1} Mbps'. format(
-                            str(chunk_counter), str(rate)))
-                        return
+        byte_counter = 0
+        conn, addr = s.accept()
+        #print("server socket accept")
+        with conn:
+            #print('Connected by', addr)
+            time_start = time.time()
+            read_num = 0
+            while True:
+                read_num += 1
+                data = conn.recv(1000)
+                if not data:
+                    break
+                byte_counter += len(data)
+                #print("server received data")
+            time_end = time.time()
+            rate = byte_counter/1_000_000/(time_end-time_start)*8
+            #print('time passed', (time_end-time_start))
+            print('received={0} KB rate={1} Mbps'. format(
+                str(byte_counter/1000), str(rate)))
 
 
 sys_argv = sys.argv
