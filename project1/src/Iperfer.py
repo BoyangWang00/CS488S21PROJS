05dcf4bf226e4cc4dededf98639dc47a2ea4b8d5
Czarina Manipon <60480872+czarinamanipon@users.noreply.github.com>
2021-03-05 19:02:34 -0500

    fixing changes

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -2,12 +2,13 @@ import sys
 import socket
 import time
 
+
 def iperf_client(sys_argv):
     if(len(sys_argv) != 4):
         print("Error: missing or additional arguments")
         quit()
 
-    if(int(sys_argv[2])<1024 or int(sys_argv[2])>65535):
+    if(int(sys_argv[2]) < 1024 or int(sys_argv[2]) > 65535):
         print("Error: port number must be in the range 1024 to 65535")
         quit()
 
@@ -16,64 +17,56 @@ def iperf_client(sys_argv):
     HOST = sys_argv[1]
     PORT = int(sys_argv[2])
     s_time = float(sys_argv[3])
-    print(s_time)
+    # print(s_time)
     chunk_counter = 0
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         data_chunk = bytearray(1000)
         timeout = time.time() + s_time
-        while (1):
+        while time.time() < timeout:
             s.sendall(data_chunk)
             chunk_counter += 1
-            #print(chunk_counter)
-            #data = s.recv(1000)
-            if time.time() > timeout:
-                s.sendall(b'1')
-                #print(time.time())
-                print("time is out")
-                break
-        s.close()
-        print(chunk_counter, "chunks are sent")
-        rate = chunk_counter/1000/s_time
+        s.sendall(b'1')
+        #print(chunk_counter, "chunks are sent")
+    rate = round(chunk_counter/1000/s_time*8,3)
     print('sent={0} KB rate={1} Mbps'. format(str(chunk_counter), str(rate)))
-    return
     #print('Received', repr(data))
+
 
 def iperf_server(sys_argv):
     if(len(sys_argv) != 3):
         print("Error: missing or additional arguments")
         quit()
-    if(int(sys_argv[2])<1024 or int(sys_argv[2])>65535):
+    if(int(sys_argv[2]) < 1024 or int(sys_argv[2]) > 65535):
         print("Error: port number must be in the range 1024 to 65535")
         quit()
-    HOST = ''                 # Symbolic name meaning all available interfaces
+    # Symbolic name meaning all available interfaces
+    HOST = ''
     PORT = int(sys_argv[2])
-    chunk_counter = 0
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        print("server socket is listening")
-        while True:
-            conn, addr = s.accept()
-            print("server socket accept")
-            with conn:
-                print('Connected by', addr)
-                time_start = time.time()
-                while True:
-                    data = conn.recv(1000)
-                    #print(data.decode())
-                    chunk_counter += 1
-                    #print("server received data")
-                    data=data.upper()
-                    if data == (b'1'):
-                        time_end = time.time()
-                        rate = chunk_counter/1000/(time_end-time_start)
-                        print('time passed' , (time_end-time_start))
-                        print('received={0} KB rate={1} Mbps'. format(str(chunk_counter), str(rate)))
-                        return
-
-#BOYANG IS HERE!!! Change return
+        #print("server socket is listening")
+        byte_counter = 0
+        conn, addr = s.accept()
+        #print("server socket accept")
+        with conn:
+            #print('Connected by', addr)
+            time_start = time.time()
+            read_num = 0
+            while True:
+                read_num += 1
+                data = conn.recv(1000)
+                if not data:
+                    break
+                byte_counter += len(data)
+                #print("server received data")
+            time_end = time.time()
+            rate = round(byte_counter/1_000_000/(time_end-time_start)*8,3)
+            #print('time passed', (time_end-time_start))
+            print('received={0} KB rate={1} Mbps'. format(
+                str(round(byte_counter/1000)), str(rate)))
 
 
 sys_argv = sys.argv
