5ca7ad71108b8c505cf6a289ed1b6c4c0e3f1a01
Boyang Wang <wangby00@gmail.com>
2021-03-26 18:59:29 -0400

    sender/receiver stop&wait

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -1,28 +1,47 @@
 # ----- receiver.py -----
 
 #!/usr/bin/env python
-
+from collections import deque
 from socket import *
 import sys
 import select
+import json
 
-host="0.0.0.0"
-port = 9999
+host= '' #"0.0.0.0"
+port = int(sys.argv[1])  #9999
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
 
 addr = (host,port)
-buf=1400
+buf=2800
 
 f = open("received.txt",'wb')
-
 data,addr = s.recvfrom(buf)
-try:
-    while(data):
-        f.write(data)
-        s.settimeout(2)
-        data,addr = s.recvfrom(buf)
-except timeout:
+
+counter = 0
+
+while True:
+  try:
+    data,addr = s.recvfrom(buf)
+    data_json = json.loads(data)
+
+    [(header, data)] = list(data_json.items())
+
+    print("header we received is {}".format(header))
+    print(type(header))
+
+    if int(header) == counter:
+        counter += 1
+        print("send counter", counter)
+        s.sendto(str(counter).encode(),addr)
+
+    f.write(data.encode()) #send to app
+    s.settimeout(10)
+
+    if not data:
+        break
+  except timeout:
+    print("time out")
     f.close()
     s.close()
     print("File Downloaded")
--- project2/src/sender.py
+++ project2/src/sender.py
@@ -4,6 +4,7 @@
 from socket import *
 import sys
 import time
+import json
 
 s = socket(AF_INET,SOCK_DGRAM)
 host = sys.argv[1]
@@ -15,21 +16,43 @@ addr = (host,port)
 total_data = 0
 
 start_time = time.time()
+header_index = 0
+akc_data = 0
+
 while True:
     try:
-        data = sys.stdin.buffer.read(buf)
+        data = sys.stdin.read(buf)
         #b_data = data.encode()
-        b_data = data
+        b_data = json.dumps({header_index:data})
+
+        #TODO: change the header index
+
     except IOError:
         print ("IOError")
 
         #stop and wait the AKC for last packet
-    if(s.sendto(b_data,addr)): #send regular packet
+    if(s.sendto(b_data.encode(),addr)): #send regular packet
         total_data += len(b_data)
         print("{} bytes have been sent ...".format(total_data))
+        try:
+            s.settimeout(2)
+            #receive AKC
+            akc_data,addr = s.recvfrom(4)
+            print("ack # is {}".format(akc_data))
 
-    if len(b_data) < buf:    # the last read will be less than 1400; we jump out of the loop
-        print("sending last packet...")
+        except timeout:
+            #resend the packet
+            s.sendto(b_data.encode(),addr)
+            print("send again b/c time out")
+        if int(akc_data) == header_index +1:
+            header_index +=1
+            continue
+        else:
+            #resned the packet
+            s.sendto(b_data.encode(),addr)
+            print("send again b/c packet lost")
+    if not b_data:    # the last read will be less than 1400; we jump out of the loop
+        print("Send finished")
         end_time = time.time()
         break
 
@@ -40,6 +63,6 @@ if time == 0:
 else:
     speed = total_data/time/1000
 
-print("Sent {} bytes in {} seconds: {} kB/s".format(total_data, time ,round(speed)))
+print("Sent {} bytes in {} seconds: {} kB/s".format(total_data, round(time) ,round(speed)))
 s.close()
 #f.close()
