fc83e2a21a057c3cdd7332a45ebc9acc7d83d57d
CJ <cjfung94@gmail.com>
2021-03-28 20:20:28 -0400

    Fixed first packet bug

--- project2/src/sender.py
+++ project2/src/sender.py
@@ -6,13 +6,13 @@ import sys
 import time
 import json
 
-s = socket(AF_INET,SOCK_DGRAM)
+s = socket(AF_INET, SOCK_DGRAM)
 host = sys.argv[1]
 port = int(sys.argv[2])
-buf = 1400   #read from stdin
-addr = (host,port)
+buf = 1400  # read from stdin
+addr = (host, port)
 
-#f=open("a.txt","rb")
+# f=open("a.txt","rb")
 total_data = 0
 
 start_time = time.time()
@@ -23,37 +23,42 @@ while True:
     try:
         data = sys.stdin.read(buf)
         #b_data = data.encode()
-        b_data = json.dumps({header_index:data})
+        b_data = json.dumps({header_index: data})
 
-        #TODO: change the header index
+        # TODO: change the header index
 
     except IOError:
-        print ("IOError")
+        print("IOError")
 
-        #stop and wait the AKC for last packet
-    if(s.sendto(b_data.encode(),addr)): #send regular packet
+        # stop and wait the AKC for last packet
+    if(s.sendto(b_data.encode(), addr)):  # send regular packet
         total_data += len(b_data)
         print("{} bytes have been sent ...".format(total_data))
         try:
             s.settimeout(2)
-            #receive AKC
-            akc_data,addr = s.recvfrom(4)
+            # receive AKC
+            akc_data, addr = s.recvfrom(5)
             print("ack # is {}".format(akc_data))
 
         except timeout:
-            #resend the packet
-            s.sendto(b_data.encode(),addr)
+            # resend the packet
+            s.sendto(b_data.encode(), addr)
             print("send again b/c time out")
-        if int(akc_data) == header_index +1:
-            header_index +=1
+        if int(akc_data) == header_index + 1:
+            header_index += 1
             continue
         else:
-            #resned the packet
-            s.sendto(b_data.encode(),addr)
+            # resned the packet
+            s.sendto(b_data.encode(), addr)
             print("send again b/c packet lost")
+    else:
+        print("failed to send data")
+        break
     if not data:    # the last read will be less than 1400; we jump out of the loop
         print("Send finished")
         end_time = time.time()
+        b_data = "-1"
+        s.sendto(b_data.encode(), addr)
         break
 
 time = (end_time - start_time)
@@ -63,6 +68,7 @@ if time == 0:
 else:
     speed = total_data/time/1000
 
-print("Sent {} bytes in {} seconds: {} kB/s".format(total_data, round(time) ,round(speed)))
+print("Sent {} bytes in {} seconds: {} kB/s".format(total_data,
+                                                    round(time), round(speed)))
 s.close()
-#f.close()
+# f.close()
