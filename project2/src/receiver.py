bbc22f21b13f516d1844136f8310aa5a05c90e95
Boyang Wang <wangby00@gmail.com>
2021-04-08 00:22:58 -0400

    more efficient code

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -16,7 +16,7 @@ s.bind((host, port))
 addr = (host, port)
 s_buf = 2800 
 
-buffer_size = 1000
+buffer_size = 40
 last_buffer_size = buffer_size
 #receiver_datagram_buffer = collections.deque(maxlen=buffer_size)
 receiver_datagram_buffer = {}
@@ -36,29 +36,35 @@ while True:
                 data_json = json.loads(b_data)  # deserialize json obj into python dict
 
                 [(header, data)] = list(data_json.items())
-                if int(header) <= receiver_buffer_round_time*buffer_size:
+                #print("header recevied is ", header)
+                #print("receiver_buffer_round ", receiver_buffer_round_time)
+                #print("last buffer_size", last_buffer_size, "num_of_items_in_buffer", num_of_items_in_buffer)
+
+
+                if int(header) < (receiver_buffer_round_time-1)*buffer_size:
                 #if we received a packet that we already write out
                 #send out ack immediantly
                     s.sendto(header.encode(), addr)
+                    #print("send ack ", header, "without put in buffer")
 
-                if((receiver_buffer_round_time-1)*buffer_size< int(header) <= receiver_buffer_round_time*buffer_size):
+                elif((receiver_buffer_round_time-1)*buffer_size<= int(header) < receiver_buffer_round_time*buffer_size):
                     #print("insert", header)
-                    position = int(header) % buffer_size
-                    if position == 0:
-                        position = buffer_size
-                    if receiver_datagram_buffer.get(position-1) == '':
-                        num_of_items_in_buffer +=1
-                        receiver_datagram_buffer[position-1] = data
                     s.sendto(header.encode(), addr)
+                    position = int(header) % buffer_size
+                    if receiver_datagram_buffer.get(position) == '':
+                        num_of_items_in_buffer +=1
+                        receiver_datagram_buffer[position] = data
+                        #print("send ack ", header,"after adding to buffer")
+
+                    if data == '':
+                        #print('                   reach end_of_file')
+                        end_of_file = True
+                        last_buffer_size = int(header) % buffer_size+1
+                        last_header = header
 
                 else:
                     pass
                     #do nothing
-
-                if b_data == '':
-                    end_of_file = True
-                    last_buffer_size = int(header) % buffer_size
-
                 s.settimeout(5)
 
             except timeout:
@@ -68,26 +74,36 @@ while True:
                         sys.stdout.write(receiver_datagram_buffer.get(i))
                         receiver_datagram_buffer[i] = ''
                         num_of_items_in_buffer -=1
-                #print("time out")
+                    #print("shoudln't end up here!!!time out!!!!!!!!!!!")
+                s.close()
                 exit(0)
 
         else: 
             #buffer is full and we need to dump 
             for i in range(buffer_size):
+                #print("buffer_size ", buffer_size, "last_buffer_size", last_buffer_size)
                 #print("line 68")
                 #print("-----------------write", i)
                 if receiver_datagram_buffer.get(i) != '':
                     sys.stdout.write(receiver_datagram_buffer.get(i))
+                    #print("write out buffer ", i)
                     receiver_datagram_buffer[i] = ''
                     num_of_items_in_buffer -=1
+                    if end_of_file == True:
+                        #print("this is last print")
+                        for i in range(10):
+                            s.sendto("-1".encode(), addr)
+                            #print("resend last ack", -1)
                     # for h in range((receiver_buffer_round_time-1)*buffer_size,receiver_buffer_round_time*buffer_size):
                     #     s.sendto(str(h).encode(), addr)
                 else:
                     #print(list(receiver_datagram_buffer.keys()))
                     #print("i is",i)
                     #print("BUG!!!!!!!! BUFFER IS NOT FULL")
-                    exit()
-            receiver_buffer_round_time +=1
+                    break
+
+            receiver_buffer_round_time += 1
+            #print("receiver_buffer_round_time increment", receiver_buffer_round_time)
             # s.sendto(header.encode(), addr)
 
 s.close()
--- project2/src/sender.py
+++ project2/src/sender.py
@@ -17,7 +17,7 @@ addr = (host, port)
 
 total_data = 0
 ack_data = 0
-buffer_size = 500
+buffer_size = 30
 sender_datagram_buffer = collections.deque(maxlen=buffer_size)
 DatagramInFlight = collections.namedtuple('DatagramInFlight', ['number','time','data'])
 datagram_number = 0
@@ -26,6 +26,7 @@ start_time = time.time()
 
 end_of_file = False
 sender_packet_count={}
+receiver_closed = False
 
 try: 
 # try to read first batch of datagram from stdin
@@ -34,7 +35,6 @@ try:
         total_data += len(data)
         
         #serilize header and data
-        datagram_number += 1
         b_data = json.dumps({datagram_number: data})
 
         s.sendto(b_data.encode(), addr)  # send regular packet
@@ -43,6 +43,7 @@ try:
 
         datagram_tuple = DatagramInFlight(number=datagram_number,time = time.time(), data = b_data)
         sender_datagram_buffer.insert(i,datagram_tuple)
+        datagram_number += 1
         #print("current data is type is ", type(data))
         if data == '':    # when we reach EOF we send out '' first and then break loop
             #print("Reach EOF")
@@ -52,46 +53,57 @@ try:
 except IOError:
     print("IOError")
 
-while len(sender_datagram_buffer) > 0:
+while len(sender_datagram_buffer) > 0 and receiver_closed != True:
     try:
         # receive AKC
         ack_data, addr = s.recvfrom(100)
         #print("akc # is {}, datagram_number is {}".format(ack_data, datagram_number))
 
-        for i in range(len(sender_datagram_buffer)):
-            #print("i is ", i ,"buffer_size is ",len(sender_datagram_buffer))
-            datagram_tuple = sender_datagram_buffer[i]
+        if ack_data.decode() == '-1':
+            #print("receiver is closed")
+            receiver_closed = True
+            break
+        else:
 
-            if int(ack_data.decode()) == datagram_tuple.number:
-                if end_of_file != True:
-                # if ack we received is for datagram in buffer and we haven't reach the EOF 
-                # fetch new data and send it out 
-                # replace old datagram with new one
-                    data = sys.stdin.read(buf)
-                    total_data += len(data)
-                    sender_datagram_buffer.remove(datagram_tuple)
-                    #serilize header and data
-                    datagram_number += 1
-                    b_data = json.dumps({datagram_number: data})
-                    s.sendto(b_data.encode(), addr)  # send regular packet
-                    sender_packet_count[datagram_number] = 1
-                    datagram_tuple_new = DatagramInFlight(number=datagram_number,time = time.time(), data = b_data)
-                    sender_datagram_buffer.insert(i,datagram_tuple_new)
+            for i in range(len(sender_datagram_buffer)):
+                #print("i is ", i ,"buffer_size is ",len(sender_datagram_buffer))
+                datagram_tuple = sender_datagram_buffer[i]
 
-                    if data == '':
-                        #After we send out the first empty string as a signal EOF
-                        #turn on end_of_file flag
-                       end_of_file = True
+                if int(ack_data.decode()) == datagram_tuple.number:
+                    if end_of_file != True:
+                    # if ack we received is for datagram in buffer and we haven't reach the EOF 
+                    # fetch new data and send it out 
+                    # replace old datagram with new one
+                        data = sys.stdin.read(buf)
+                        total_data += len(data)
+                        sender_datagram_buffer.remove(datagram_tuple)
+                        if data == '':
+                            #After we send out the first empty string as a signal EOF
+                            #turn on end_of_file flag
+                            end_of_file = True
+                            b_data = json.dumps({datagram_number: data})
+                            s.sendto(b_data.encode(), addr)  # send regular packet
+                            sender_packet_count[datagram_number] = 1
+                        else:
+                            #serilize header and data
+                            b_data = json.dumps({datagram_number: data})
+                            s.sendto(b_data.encode(), addr)  # send regular packet
+                            sender_packet_count[datagram_number] = 1
+                        datagram_tuple_new = DatagramInFlight(number=datagram_number,time = time.time(), data = b_data)
+                        sender_datagram_buffer.insert(i,datagram_tuple_new)
+                        datagram_number += 1
+
+
+                    else:
+                    # we already reached EOF and already sent out ''; 
+                    # thus only remove datagram from buffer
+                        sender_datagram_buffer.remove(datagram_tuple)
+
+                    #after taking care of ack break out loop.
+                    break
                 else:
-                # we already reached EOF and already sent out ''; 
-                # thus only remove datagram from buffer
-                    sender_datagram_buffer.remove(datagram_tuple)
-
-                #after taking care of ack break out loop.
-                break
-            else:
-                #if ACK ! = datagram_tuple.number, check next datagram_tuple in buffer.
-                pass
+                    #if ACK ! = datagram_tuple.number, check next datagram_tuple in buffer.
+                    pass
 
     except socket.error as e:
         err = e.args[0]
@@ -105,7 +117,7 @@ while len(sender_datagram_buffer) > 0:
                         b_data = datagram_tuple.data
                         sender_datagram_buffer[i] = sender_datagram_buffer[i]._replace(time = resend_time)
                         assert sender_datagram_buffer[i].time == resend_time
-                        #print("datagram number is", datagram_tuple.number)
+                        #print("resend b/c time out datagram number is", datagram_tuple.number)
                         # resend the packet
                         s.sendto(b_data.encode(), addr)
                         sender_packet_count[datagram_tuple.number] += 1
