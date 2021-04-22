d3a43771f7c4e1f213e54cd614afc4d00b576bb9
CJ <cjfung94@gmail.com>
2021-04-22 19:30:20 -0400

    BSOD

--- project3/src/client.py
+++ project3/src/client.py
@@ -5,12 +5,13 @@ import socket
 import sys
 import os
 
-#Client has old file β 
+# Client has old file β
 BLOCK_SIZE = 4
 
 # Hasher
 # Helper functions
 # ----------------
+
 
 def md5_chunk(chunk):
     """
@@ -32,11 +33,14 @@ def adler32_chunk(chunk):
 # ----------------
 Signature = collections.namedtuple('Signature', 'md5 adler32')
 
-#Chunks = a list of Signatures
+# Chunks = a list of Signatures
+
+
 class Chunks(object):
     """
     Data stucture that holds rolling checksums for file B
     """
+
     def __init__(self):
         self.chunks = []
         self.chunk_sigs = {}
@@ -62,6 +66,8 @@ class Chunks(object):
 
 # Build Chunks from a file
 # ------------------------
+
+
 def checksums_file(fn):
     """
     Returns object with checksums of file
@@ -78,7 +84,7 @@ def checksums_file(fn):
                 Signature(
                     adler32=adler32_chunk(chunk.encode()),
                     md5=md5_chunk(chunk.encode()),
-                    offset = fn_offset
+                    offset=fn_offset
                 )
             )
 
@@ -87,21 +93,20 @@ def checksums_file(fn):
         return chunks
 
 
-#TODO: FINISH THIS FUNCTION
+# TODO: FINISH THIS FUNCTION
 # reconstruct the NEW file by using OLD file, OLD_TEMP file and checksums list received from server
 
-def reconstruct_file(OLD, TEMP_LOG, server_list, old_list, temp_log_list):
-    with open('NEW_TEMP', 'w') as new_temp:
-        for block in server_list:
-            if block in old_list:
-                #find block with offset and write out to new_temp file
-            elif block in temp_log_list:
-                #find block with offset and write out to new_temp file
-    return new_temp
+# def reconstruct_file(OLD, TEMP_LOG, server_list, old_list, temp_log_list):
+#     with open('NEW_TEMP', 'w') as new_temp:
+#         for block in server_list:
+#             if block in old_list:
+#                 # find block with offset and write out to new_temp file
+#             elif block in temp_log_list:
+#                 # find block with offset and write out to new_temp file
+#     return new_temp
 
 
-
-#Client pass in server @ and port in commandline [1][2]
+# Client pass in server @ and port in commandline [1][2]
 serverName = sys.argv[1]
 serverPort = int(sys.argv[2])
 serverAddress = (serverName, serverPort)
@@ -109,16 +114,16 @@ serverAddress = (serverName, serverPort)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
     clientSocket.connect(serverAddress)
-                #clientSocket.setblocking(0) # client non-blocking to receive list from server (?)
+    # clientSocket.setblocking(0) # client non-blocking to receive list from server (?)
     # Client needs to send server a signal that it wants to update
     clientSocket.send('s'.encode())
 
     # Receive List from the server = ChunkList
     received_data = b''
     while True:
-    #call a while loop to receve all the data send by server,
-    #if server reach to EOF, clientSocket.recv() will return 0, break the loop
-        data = clientSocket.recv(1024) #how many B recv?
+        # call a while loop to receve all the data send by server,
+        # if server reach to EOF, clientSocket.recv() will return 0, break the loop
+        data = clientSocket.recv(1024)  # how many B recv?
         if data:
             received_data += data
         else:
@@ -131,14 +136,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
 
 # If it is not, offset by 1 and check
 
-
-    checksums =  json.loads(received_data.decode(encoding='utf-8')) # decode from the server and you get the list of hashes
+    # decode from the server and you get the list of hashes
+    checksums = json.loads(received_data.decode(
+        encoding='utf-8'))  # Returns the list
     # Make a copy of the checksums List = IE. localChecksums
     localChecksums = checksums.copy()
+    list_to_write = []
     offset = 0
     with open('OLD') as f:
         while True:
             chunk = f.read(BLOCK_SIZE)
+            # list_to_write.append(chunk)
             # Until EOF
             if not chunk:
                 break
@@ -149,16 +157,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
             if chunk_number is not None:
                 offset += BLOCK_SIZE
                 localChecksums.remove(chunk_number)
-                continue
+                # continue
                 # Just offset by one, read from that part of the file, and then move on
             else:
                 offset += 1
                 f.seek(offset)
-                continue
+                # continue
 
-#TODO: need to add code to check whether there is a temp_log file under current directry.
-#if there is, which means the download was interrupted last time, we need to check both OLD file 
-#and TEMP_LOG file 
+# TODO: need to add code to check whether there is a temp_log file under current directry.
+# if there is, which means the download was interrupted last time, we need to check both OLD file
+# and TEMP_LOG file
     try:
         with open('TEMP_LOG') as temp_log:
             while True:
@@ -173,16 +181,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
                 if chunk_number is not None:
                     offset += BLOCK_SIZE
                     localChecksums.remove(chunk_number)
-                    continue
+                    # continue
                     # Just offset by one, read from that part of the file, and then move on
                 else:
-                    continue
+                    # continue
                     print("download was interrupted before")
-                    #TEMP_LOG should not fall into this branch unless it reach the last short block 
-                    #because everything else in TEMP_LOG are requested blocks from server
+                    # TEMP_LOG should not fall into this branch unless it reach the last short block
+                    # because everything else in TEMP_LOG are requested blocks from server
     except OSError:
         print('no temp_log in current directry')
-
 
 
 # After comparation is done, then send the request list to the server
@@ -190,45 +197,71 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
 # client.sendto(server)
 
     request_list = json.dumps(localChecksums)
+    # Why not sendall???
     clientSocket.send(request_list.encode())
 
     while localChecksums:
 
-    # client start to receiv data chuncks from server
+        # client start to receiv data chuncks from server
         received_chunck = b''
         while True:
-        #call a while loop to receve all the data send by server,
-        #if server reach to EOF, clientSocket.recv() will return 0, break the loop
-            data = clientSocket.recv(1024) #how many B recv?
-            if data:
+            # call a while loop to receve all the data send by server,
+            # if server reach to EOF, clientSocket.recv() will return 0, break the loop
+            data = clientSocket.recv(1024)  # how many B recv?
+            if data and data.decode() != '':
                 received_data += data
+
+                # Turn data you received into a Hash into a new variable
+
+                # Check the client list and see if it's inside the client's list
+                # If it isn't, then check the server's list to see if it's inside
+                # If it's inside the servers, then find the position
+                if x in request_list:
+                    pass
+                else:
+                    if x in server_list:
+                        # save position of new data to updatae
+                        updateAt = server_list.index(x)
+                    else:
+                        print("{} is not in server list".format(chunk))
+                # Left off here
+                list_to_write.append(index, data.decode())
+
+                # Using the position, append the data into the position for list_to_write
+
             else:
                 break
+    with open('OLD', 'w') as write_out:
+        # Converts to string
+        list_to_write = "".join(list_to_write)
+        # Writes entire thing to file
+        write_out.write(list_to_write)
 
-        #if file_size%BLOCK_SIZE == 0, no short chunks in the file
-        #append received data by the end
-        #else reset the offset to the end of last whole chunk
-        #overwrite the short chunck
+        # if file_size%BLOCK_SIZE == 0, no short chunks in the file
+        # append received data by the end
+        # else reset the offset to the end of last whole chunk
+        # overwrite the short chunck
         file_size = os.stat('TEMP_LOG').st_size
         if file_size % BLOCK_SIZE == 0
-            with open('TEMP_LOG', 'a') as temp_log:
-                temp_log.write(received_data.decode())
+        with open('TEMP_LOG', 'a') as temp_log:
+            temp_log.write(received_data.decode())
         else:
             with open('TEMP_LOG', 'r+') as temp_log:
                 temp_log.seek(file_size//BLOCK_SIZE*BLOCK_SIZE)
                 temp_log.write(received_data.decode())
 
-        #after write out the received chunk data, remove it from list
+        # after write out the received chunk data, remove it from list
         localChecksums.remove(checksums.get_chunk(received_data))
 
 # at this point, everything client requested for is saved in OLD_TEMP file,
 # we can close the TCP connection and start re-contruct the NEW file at client's end
 
-new_temp = reconstruct_file(OLD, TEMP_LOG,checksums,checksums_file(OLD), checksums_file(TEMP_LOG))
+new_temp = reconstruct_file(
+    OLD, TEMP_LOG, checksums, checksums_file(OLD), checksums_file(TEMP_LOG))
 
-#TODO!!
-#rename new_temp to replace OLD file
-#delete OLD_TEMP file
+# TODO!!
+# rename new_temp to replace OLD file
+# delete OLD_TEMP file
 
 
 # Client will then decode and construct the new file according to checksums' order
--- project3/src/server.py
+++ project3/src/server.py
@@ -5,7 +5,7 @@ import socket
 import sys
 import json
 
-#Server has new file α
+# Server has new file α
 BLOCK_SIZE = 4
 
 # Hasher
@@ -57,8 +57,9 @@ class Chunks(object):
         return None
 
     def get_offset(self, md5):
-        offset = [self.chunks.offset for signature in self.chunks if self.chunks.md5 == md5]
-        assert len(offset)==1
+        offset = [
+            self.chunks.offset for signature in self.chunks if self.chunks.md5 == md5]
+        assert len(offset) == 1
         return offset[0]
 
     def __getitem__(self, idx):
@@ -86,7 +87,7 @@ def checksums_file(fn):
                 Signature(
                     adler32=adler32_chunk(chunk.encode()),
                     md5=md5_chunk(chunk.encode()),
-                    offset = fn_offset
+                    offset=fn_offset
                 )
             )
 
@@ -101,15 +102,15 @@ ServerAddress = (ServerName, ServerPort)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
     serverSocket.bind(ServerAddress)
-    serverSocket.listen(1) #queue up 1 connection request
+    serverSocket.listen(1)  # queue up 1 connection request
     print('The server is listening')
     connection_socket, addr = serverSocket.accept()
-    
+
     while 1:
         # Server will receive the signal that client wants to update
         msg = connection_socket.recv(1024).decode()  # or 1 byte? try catch?
         if not msg:
-            break 
+            break
 
     print("First signal is received")
     # Call checksumfiles to make the NEW block list
@@ -117,50 +118,56 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
 
     # Send checksums_file (which is the hashed list of the file) to client
 
-    
-    str_data = json.dumps(chunkList) #Necessary? create str from json-like-Python dict
-    connection_socket.sendall(bytes(str_data, encoding="utf-8")) #send entire buffer, sendto() is only for UDP datagram
+    # Necessary? create str from json-like-Python dict
+    str_data = json.dumps(chunkList)
+    # send entire buffer, sendto() is only for UDP datagram
+    connection_socket.sendall(bytes(str_data, encoding="utf-8"))
 
     # Server receives List from client
 
     received_request = b''
     while True:
-    #call a while loop to receve all the data send by server,
-    #if server reach to EOF, serversocket.recv() will return 0, break the loop
+        # call a while loop to receve all the data send by server,
+        # if server reach to EOF, serversocket.recv() will return 0, break the loop
         data = connection_socket.recv(1024)
         if data:
             received_request += data
         else:
             break
-    #receive request chuncks from client
+    # receive request chuncks from client
     request_checksums = json.loads(received_request.decode(encoding="utf-8"))
-    #find the offset of the chuncks and form offset list
-    offset_list = [chunkList.get_offset(signature.md5) for signature in request_checksums.chunks]
+    # find the offset of the chuncks and form offset list
+    # .Chunks should not be iterable
+    offset_list = [chunkList.get_offset(signature.md5)
+                   for signature in request_checksums.chunks]
 
-    #Server will only send the chunks that client is requesting for
-    #open file again and load requested chuncks by offset and send it over to client
+    # Server will only send the chunks that client is requesting for
+    # open file again and load requested chuncks by offset and send it over to client
+    last_chunk = ''
     with open('NEW') as f:
         for offset in offset_list:
             f.seek(offset)
             chunk = f.read(BLOCK_SIZE)
-            #we shouldn't fall into this branch because requested chuncks is 
-            #part of NEW file
+            # we shouldn't fall into this branch because requested chuncks is
+            # part of NEW file
             if not chunk:
                 print("BLOCK DOESN'T EXIST!")
                 assert(False)
-            #if len(chunk) is less than block size, we need to hold this block
-            #send it over at last
-            #client will write out the received chunks to a temp file upon receiving
-            #if client will always use len(tempfile)%BLOCK_SIZE == 0 to check whether 
-            #the writing process is interruped in the middle or not
-            #thus we cannot send over the short chuncks until we finish sending others
+            # if len(chunk) is less than block size, we need to hold this block
+            # send it over at last
+            # client will write out the received chunks to a temp file upon receiving
+            # if client will always use len(tempfile)%BLOCK_SIZE == 0 to check whether
+            # the writing process is interruped in the middle or not
+            # thus we cannot send over the short chuncks until we finish sending others
             if len(chunk) < BLOCK_SIZE:
                 last_chunk = chunk
                 continue
+            else:
 
-            connection_socket.sendall(chunk.encode())
+                connection_socket.sendall(chunk.encode())
+
+        # Why do we need this?
         connection_socket.sendall(last_chunk.encode())
-
 
     # May not need the following steps- BW
     # Server will assign a header or a tracker to each block size of bytes that it will send to the Client
